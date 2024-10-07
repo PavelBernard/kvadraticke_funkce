ef911930f9759054bdb5b2012374cdd0c2c61d08
Janeček Pavel <L20220039@SSPTAJI.CZ>
2024-10-07 13:09:53 +0200

    zprava

--- app.js
+++ app.js
@@ -10,6 +10,28 @@ if ('serviceWorker' in navigator) {
     });
 }
  
-function sayHello() {
-    document.getElementById('hello').innerText = 'Hello world';
+function calculate() {
+    const a = document.getElementById("a").value;
+    const b = document.getElementById("b").value;
+    const c = document.getElementById("c").value;
+
+    let res1;
+    let res2;
+
+    let disc = (b * b) - (4 * a * c)
+
+    if (disc >= 0){
+        if  (d = 0){
+            res1 = (-b) / (2 * a)
+            res2 = (-b) / (2 * a)
+        }
+        res1 = (-b + Math.sqrt(disc)) / (2 * a);
+        res2 = (-b - Math.sqrt(disc)) / (2 * a);
+    }
+    else{
+        res1 = "nelze najit vysledek v poli realnych cisel";
+    }
+
+    document.getElementById(x1).innerText = res1;
+    document.getElementById(x2).innerText = res2;
 }
--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,37 @@
-// This code executes in its own worker or thread
-self.addEventListener("install", event => {
-    console.log("Service worker installed");
- });
- self.addEventListener("activate", event => {
-    console.log("Service worker activated");
- });
+// Registrace service workeru
+if ('serviceWorker' in navigator) {
+   window.addEventListener('load', () => {
+   navigator.serviceWorker.register('/sw.js').then(registration => {
+   console.log('Service Worker registrovan s nasledujicim scope:', registration.scope);
+   }, err => {
+   console.log('Service Worker registrace selhala:', err);
+   });
+   });
+   }
+   // Instalace service workeru
+   self.addEventListener('install', event => {
+   console.log('Service Worker se instaluje...');
+   event.waitUntil(
+   caches.open('my-cache').then(cache => {
+   return cache.addAll([
+   '/',
+   '/index.html',
+   '/style.css',
+   '/script.js'
+   ]);
+   })
+   );
+   });
+   // Aktivace service workeru
+   self.addEventListener('activate', event => {
+   console.log('Service Worker byl aktivovan');
+   });
+   // Zachyt´av´an´ı poˇzadavk˚u
+   self.addEventListener('fetch', event => {
+   event.respondWith(
+   caches.match(event.request).then(response => {
+   return response || fetch(event.request);
+   })
+   );
+   });
+   
